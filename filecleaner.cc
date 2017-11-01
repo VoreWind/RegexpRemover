16a40c00ad8db0ddf4d160136d2f563655eda9d8
WereWind <serg-ef@mail.ru>
2017-11-01 19:47:40 +0300

    Add a check to ignore the files without the regexp

--- filecleaner.cc
+++ filecleaner.cc
@@ -9,16 +9,23 @@
 
 void FileCleaner::ClearRegExpFromFile(const QString &file_path,
                                       const QString &regexp_string) {
+  QRegExp regexp(regexp_string);
+  if (!regexp.isValid()) {
+    return;
+  }
+
   QFile source_file(file_path);
   source_file.open(QIODevice::ReadWrite);
 
-  QRegExp regexp(regexp_string);
   regexp.setMinimal(true);
   QTextStream in_out(&source_file);
   in_out.setCodec("UTF-8");
   QString file_contents = in_out.readAll();
 
-  qDebug() << file_contents.indexOf(regexp) << regexp.isValid();
+  if (file_contents.indexOf(regexp) == -1) {
+    return;
+  }
+
   file_contents.remove(regexp);
   source_file.resize(0);
   in_out << file_contents;
